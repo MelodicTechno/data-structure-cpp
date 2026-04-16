cb36c6f1675a31ee3a8d7318a1504be18ede8c7e
MelodicTechno <617179025@qq.com>
2026-04-16 10:11:36 +0800

    ref: modifiy cmakelists

---
+++ src/demo/matplot.cpp
@@ -0,0 +1,17 @@
+#include <matplot/matplot.h>
+
+int main() {
+    using namespace matplot;
+    std::vector<double> x = linspace(0, 2 * pi, 200);
+    std::vector<double> y;
+    y.reserve(x.size());
+    for (double v : x) {
+        y.push_back(std::sin(v));
+    }
+
+    plot(x, y);
+    title("y = sin(x)");
+    xlabel("x");
+    ylabel("y");
+    show();
+}
